136aed319b2c3e9f67e226493d4752b26fb8b355
Stjepan Mikulić <smikul00@fesb.hr>
2020-07-22 20:49:44 +0200

    dodane 3 tocke

---
+++ UDP_PCESP_komunikacija/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace UDP_PCESP_komunikacija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.IPracunala = new System.Windows.Forms.TextBox();
+            this.IPodredista = new System.Windows.Forms.TextBox();
+            this.PORT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Zapocni = new System.Windows.Forms.Button();
+            this.slider = new System.Windows.Forms.TrackBar();
+            this.tipka1 = new System.Windows.Forms.Button();
+            this.tipka2 = new System.Windows.Forms.Button();
+            this.tipka3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IPracunala
+            // 
+            this.IPracunala.Enabled = false;
+            this.IPracunala.Location = new System.Drawing.Point(150, 26);
+            this.IPracunala.Name = "IPracunala";
+            this.IPracunala.Size = new System.Drawing.Size(144, 20);
+            this.IPracunala.TabIndex = 0;
+            this.IPracunala.TextChanged += new System.EventHandler(this.IPracunala_TextChanged);
+            // 
+            // IPodredista
+            // 
+            this.IPodredista.Location = new System.Drawing.Point(150, 69);
+            this.IPodredista.Name = "IPodredista";
+            this.IPodredista.Size = new System.Drawing.Size(144, 20);
+            this.IPodredista.TabIndex = 1;
+            this.IPodredista.TextChanged += new System.EventHandler(this.IPodredista_TextChanged);
+            // 
+            // PORT
+            // 
+            this.PORT.Location = new System.Drawing.Point(171, 109);
+            this.PORT.Name = "PORT";
+            this.PORT.Size = new System.Drawing.Size(110, 20);
+            this.PORT.TabIndex = 2;
+            this.PORT.TextChanged += new System.EventHandler(this.PORT_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP adresa racunala";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "IP adresa odredista";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PORT";
+            // 
+            // Zapocni
+            // 
+            this.Zapocni.Location = new System.Drawing.Point(190, 155);
+            this.Zapocni.Name = "Zapocni";
+            this.Zapocni.Size = new System.Drawing.Size(75, 23);
+            this.Zapocni.TabIndex = 6;
+            this.Zapocni.Text = "Zapocni";
+            this.Zapocni.UseVisualStyleBackColor = true;
+            this.Zapocni.Click += new System.EventHandler(this.Zapocni_Click);
+            // 
+            // slider
+            // 
+            this.slider.Location = new System.Drawing.Point(97, 207);
+            this.slider.Maximum = 100;
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(266, 45);
+            this.slider.TabIndex = 8;
+            this.slider.Value = 50;
+            this.slider.Scroll += new System.EventHandler(this.slider_Scroll);
+            // 
+            // tipka1
+            // 
+            this.tipka1.Location = new System.Drawing.Point(190, 271);
+            this.tipka1.Name = "tipka1";
+            this.tipka1.Size = new System.Drawing.Size(75, 23);
+            this.tipka1.TabIndex = 9;
+            this.tipka1.Text = "Sklopka  1";
+            this.tipka1.UseVisualStyleBackColor = true;
+            this.tipka1.Click += new System.EventHandler(this.tipka1_Click);
+            // 
+            // tipka2
+            // 
+            this.tipka2.Location = new System.Drawing.Point(190, 315);
+            this.tipka2.Name = "tipka2";
+            this.tipka2.Size = new System.Drawing.Size(75, 23);
+            this.tipka2.TabIndex = 10;
+            this.tipka2.Text = "Sklopka  2";
+            this.tipka2.UseVisualStyleBackColor = true;
+            this.tipka2.Click += new System.EventHandler(this.tipka2_Click);
+            // 
+            // tipka3
+            // 
+            this.tipka3.Location = new System.Drawing.Point(190, 357);
+            this.tipka3.Name = "tipka3";
+            this.tipka3.Size = new System.Drawing.Size(75, 23);
+            this.tipka3.TabIndex = 11;
+            this.tipka3.Text = "Sklopka  3";
+            this.tipka3.UseVisualStyleBackColor = true;
+            this.tipka3.Click += new System.EventHandler(this.tipka3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(437, 450);
+            this.Controls.Add(this.tipka3);
+            this.Controls.Add(this.tipka2);
+            this.Controls.Add(this.tipka1);
+            this.Controls.Add(this.slider);
+            this.Controls.Add(this.Zapocni);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PORT);
+            this.Controls.Add(this.IPodredista);
+            this.Controls.Add(this.IPracunala);
+            this.Name = "Form1";
+            this.Text = "Kontrola rasvjete";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IPracunala;
+        private System.Windows.Forms.TextBox IPodredista;
+        private System.Windows.Forms.TextBox PORT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Zapocni;
+        private System.Windows.Forms.TrackBar slider;
+        private System.Windows.Forms.Button tipka1;
+        private System.Windows.Forms.Button tipka2;
+        private System.Windows.Forms.Button tipka3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ UDP_PCESP_komunikacija/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;     // biblioteke za sockete slanje/primanje udp paketa
+
+
+
+namespace UDP_PCESP_komunikacija
+{
+    public partial class Form1 : Form
+
+    {
+
+
+        UdpClient klijent;                         // defaultni konstruktori
+        IPEndPoint odrediste;
+
+        int TIPKA1 = 0, TIPKA2 = 0, TIPKA3 = 0;  // ovisno je li tipka uprita vraca true ili false
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPracunala.Text = getIPAddress();   // cita ip adresu racunala, te je ispisuje u textbox
+
+          
+            
+
+        }
+
+        private void IPracunala_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IPodredista_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PORT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Zapocni_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+           odrediste = new IPEndPoint(IPAddress.Parse(IPodredista.Text), int.Parse(PORT.Text));    // ip i port odredišta
+
+            klijent = new UdpClient();                      // konstruktor za Klijent-a
+
+            klijent.Connect(odrediste);                       // slanje paketa na odredište u kojem su prosljeđeni parametri ip adrese i port
+
+
+            timer1.Start();
+
+        }
+
+
+
+
+      
+
+        private void slider_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tipka1_Click(object sender, EventArgs e)
+        {
+
+            if(TIPKA1 == 0)
+               TIPKA1 = 1;
+            else
+               TIPKA1 = 0;
+
+
+        }
+
+        private void tipka2_Click(object sender, EventArgs e)
+        {
+            if (TIPKA2 == 0)
+                TIPKA2 = 1;
+            else
+                TIPKA2 = 0;
+
+        }
+
+        private void tipka3_Click(object sender, EventArgs e)
+        {
+            if (TIPKA3 == 0)
+                TIPKA3 = 1;
+            else
+                TIPKA3 = 0;
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+           // string a =  slider.Value.ToString(); // uzima vrijednost slidera te prebacuje int u string
+            string b =  TIPKA1.ToString();
+            string c =  TIPKA2.ToString();
+            string d =  TIPKA3.ToString();
+
+            string poruka = b + c + d;
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(poruka);  // pakira podatke "poruke", u oblik pogodan za slanje internetom 
+
+            klijent.Send(sendBytes, sendBytes.Length);   // slanje podataka koji se nalaze u string varijabli "poruka"
+
+ 
+        }
+
+        private string getIPAddress()
+        {
+
+            //Get all local IP addresses
+            IPHostEntry ipe = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] ip = ipe.AddressList;
+            for (int i = 0; i < ip.Length; i++)
+            {
+                if (ip[i].AddressFamily.ToString().Equals("InterNetwork"))
+                {
+
+                    return ip[i].ToString();
+                }
+            }
+            return null;
+        }
+
+
+     
+
+    }
+}
